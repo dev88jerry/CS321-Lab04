11d758e22dbf75069869fdf08dc8472f0fcd479b
dev88jerry <70291890+dev88jerry@users.noreply.github.com>
2022-02-08 17:51:02 -0500

    Update lab4a.java

--- lab4a.java
+++ lab4a.java
@@ -1,96 +1,122 @@
-import java.util.Collections;
-//missing package otherwise it should be good
-public class Main
-{
-	public static void main(String[] args) {
+package Lab04A;
 
-		Person p = new Person("abc", 1);
-		Person p1 = new Person("abcd", 102);
-		Person p2 = new Person("xyz", 128);
-		Person p3 = new Person("wxyz", 20);
-		Person p4 = new Person("qwerty", 88);
-		
-		Person[] myList = new Person[5];
-		
-		myList[0] = p;
-		myList[1] = p1;
-		myList[2] = p2;
-		myList[3] = p3;
-		myList[4] = p4;
-		
-		for (Person myP : myList){
-		    System.out.println(myP.toString());
-		}
-		
-		
-		Collections.sort(myList, new LexicographicComparator());
-		
-		System.out.println("After sorting");
-		for (Person myP : myList){
-		    System.out.println(myP.toString());
-		}
-		
-		
-	}
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person p = new Person("abc", 1);
+        Person p1 = new Person("abcd", 102);
+        Person p2 = new Person("xyz", 128);
+        Person p3 = new Person("wxyz", 20);
+        Person p4 = new Person("qwerty", 88);
+
+        List<Person> people = Arrays.asList(p, p1, p2, p3, p4);
+
+        System.out.println("Initial list of people");
+        for (Person myP : people) {
+            System.out.println(myP.toString());
+        }
+        System.out.println("");
+
+        Collections.sort(people, new LexicographicComparator());
+
+        System.out.println("After sorting by name");
+        System.out.println("");
+        for (Person myP : people) {
+            System.out.println(myP.toString());
+        }
+        System.out.println("");
+
+        Collections.sort(people, new AgeComparator());
+
+        System.out.println("After sorting by age");
+        System.out.println("");
+        for (Person myP : people) {
+            System.out.println(myP.toString());
+        }
+    }
 }
 
 
-public class Person{
-    
+/*
+New class
+*/
+
+package Lab04A;
+
+public class Person {
+
     private String name;
     private int age;
-    
-    
-    public Person(){
+
+
+    public Person() {
         this.name = "";
         this.age = 0;
     }
-    
-    public Person(String name, int age){
+
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
-    
-    
-    public String getName(){
+
+
+    public String getName() {
         return this.name;
     }
-    
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
-    
-    public int getAge(){
+
+    public int getAge() {
         return this.age;
     }
-    
-    public void setAge(int age){
+
+    public void setAge(int age) {
         this.age = age;
     }
-    
-    public String toString(){
+
+    public String toString() {
         return this.name + " is age " + this.age;
     }
-    
-    
+
 }
 
-import java.util.Comparator;
-//https://www.geeksforgeeks.org/comparator-interface-java/
-public class AgeComparator implements Comparator{
-    
-    public int compare(Object arg0, Object, arg1){
-        return (Person) arg0.getAge() - (Person) arg1.getAge();
-    }
-    
-}
+
+/*
+New class
+*/
+
+package Lab04A;
 
 import java.util.Comparator;
 
-public class LexicographicComparator implements Comparator{
-    
-    public int compare(Object arg0, Object, arg1){
-        return (Person) arg0.getName().compareTo((Person) arg1.getName());
+public class AgeComparator implements Comparator<Person>{
+
+    @Override
+    public int compare(Person o1, Person o2) {
+        return o1.getAge() - o2.getAge();
     }
-    
+}
+
+
+/*
+New class
+*/
+
+package Lab04A;
+
+import java.util.Comparator;
+
+public class LexicographicComparator implements Comparator<Person>{
+
+    @Override
+    public int compare(Person o1, Person o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
 }
